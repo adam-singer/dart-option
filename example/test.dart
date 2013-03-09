268b68d487cd70c2f8c63fd30e6c42de4b8790d4
Joseph Moniz <joseph.moniz@gmail.com>
2013-03-09 09:30:56 -0800

    created examples

---
+++ example/test.dart
@@ -0,0 +1,17 @@
+import '../lib/option.dart';
+
+main() {
+  final some   = new Some(42);
+  final inner  = some.get();
+  final mapped = some.map((n) => n / 2);
+  final toSome = some.flatMap((n) => new Some(12));
+  final toNone = some.flatMap((n) => new None<num>());
+  final passed = some.filter((n) => true);
+  final failed = some.filter((n) => false);
+  final none   = new None();
+  final alt1   = none.getOrElse(42);
+  final alt2   = none.orElse(() => new Some(33));
+  final alt3   = none.orElse(new Some(32));
+
+  print('secret to all ${some.getOrElse(12)}');
+}
